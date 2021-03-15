a4e8356541c1d10dbea35d6499aa97bb1d544fa6
Kenny Wilde <kendevwilde@gmail.com>
2021-03-15 12:58:03 +0000

    handles behaviour if user rejects geolocation api

--- assets/js/geolocation.js
+++ assets/js/geolocation.js
@@ -1,9 +1,16 @@
 const getDeviceCoordinates = async () => {
+  const rejected = () => {
+    alert("Geolocation is not supported by your browser or is not enabled.");
+    document.querySelector(".how-to-container").classList.remove("hide");
+    document.querySelector(".spinner-border").classList.toggle("hide");
+    return;
+  };
+
   if (!navigator.geolocation) {
     alert("Geolocation is not supported by your browser or is not enabled.");
   } else {
     const pos = await new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition(resolve, reject);
+      navigator.geolocation.getCurrentPosition(resolve, rejected);
     });
 
     return {
